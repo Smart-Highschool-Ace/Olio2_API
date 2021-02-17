7ead18d3e8153fafbd9308138bbc1feda8f9a870
hmoon630 <s19017@gsm.hs.kr>
2021-02-17 17:14:06 +0900

    [ADD] Nexus User Schema Example

--- src/schema/types/user.ts
+++ src/schema/types/user.ts
@@ -1,23 +1,38 @@
+import { PrismaClient } from "@prisma/client";
 import { objectType, queryType, intArg } from "nexus";
+
+const prisma = new PrismaClient();
 
 export const User = objectType({
   name: "User",
   definition(t) {
-    t.id("id");
+    t.int("id");
     t.string("name");
-    t.string("email");
-    t.string("password");
-    t.string("createdAt");
+    t.string("school");
+    t.string("profile_image");
+    t.string("introduction");
+    t.int("entrance_year");
+    t.int("grade", {
+      resolve: (root) => {
+        return new Date().getFullYear() - root.entrance_year;
+      },
+    });
   },
 });
 
 export const Query = queryType({
   definition(t) {
+    t.list.field("allUser", {
+      type: User,
+      resolve: async () => {
+        return await prisma.user.findMany();
+      },
+    });
     t.field("user", {
       type: User,
       args: { id: intArg() },
-      async resolve(_root, { id }, ctx) {
-        return await ctx.prisma.user.findFirst({
+      resolve: async (_, { id }, __) => {
+        return await prisma.user.findFirst({
           where: {
             id: id,
           },
