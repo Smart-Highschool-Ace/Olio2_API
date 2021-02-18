4a77c75a7cc4bc018f54e0c1a1dab7154db3cffa
hanbin8269 <hanbin8269@gmail.com>
2021-02-19 08:27:01 +0900

    [ADD] 포트폴리오 수정 뮤테이션 생성

--- src/schema/types/portfolio.ts
+++ src/schema/types/portfolio.ts
@@ -1,6 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 import { objectType, queryType, extendType, inputObjectType, arg, nonNull, intArg } from "nexus";
 
+export const MutationPortfolio = extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.nonNull.field('updatePortfolio', {
+      type: 'String',
+      args: {
+        user : arg({type : PortfolioUpdateInput})
+      },
+      resolve(_, args, ctx) { // 후에 포트폴리오 수정 구현
+        const mock_link = "http://mock-example.com"
+        return mock_link 
+      }
+    })
+  }
+});
+
 const PortfolioUpdateInput = inputObjectType({
   name : 'PortfolioUpdateInput',
   definition(t){
