230df1e34058ab565f586e5a523e408117ac646d
hanbin8269 <hanbin8269@gmail.com>
2021-02-20 00:04:27 +0900

    [FIX] updatePortfolio arguments name

--- src/schema/types/portfolio/mutations.ts
+++ src/schema/types/portfolio/mutations.ts
@@ -4,7 +4,7 @@ export const MutationPortfolio = mutationType({
   definition(t) {
     t.nonNull.field('updatePortfolio', {
       args: {
-        user : arg({type : 'PortfolioUpdateInput'})
+        portfolio : arg({type : 'PortfolioUpdateInput'})
       },
       resolve: (_, args, ctx) => { // 후에 포트폴리오 수정 구현
         const mock_link = "http://mock-example.com"
