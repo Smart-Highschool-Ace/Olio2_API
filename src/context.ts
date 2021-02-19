993c91eeb2fb074c0b08cb34f242f3a8c9f6c88d
sunrabbit123 <qudwls185@naver.com>
2021-02-20 02:06:22 +0900

    Update context interface

---
+++ src/context.ts
@@ -0,0 +1,28 @@
+import { Context } from 'koa';
+
+import { PrismaClient } from '@prisma/client';
+
+import { decodeToken } from 'util/token';
+
+export interface context {
+    userId : Number;
+}
+
+export let createContext = async (ctx : Context) => {
+    const prisma = new PrismaClient();
+    console.log(ctx.header)
+
+    if (ctx.header.Authorization) {
+        const { userId } = decodeToken(ctx.header.Authorization);
+
+        const user = await prisma.user.findFirst({
+            where: {
+                id : userId,
+            },
+        });
+
+        if (user) {
+            return { userId : user.id };
+        }
+    }
+};
