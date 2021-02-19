a0869aff626e05781f2f2b0b2fdb01ea73b4b164
hanbin8269 <hanbin8269@gmail.com>
2021-02-20 01:08:01 +0900

    [ADD] 프로젝트 update, delete, 좋아요 기능 뮤테이션 틀 확보

--- src/schema/types/mutations/Project.ts
+++ src/schema/types/mutations/Project.ts
@@ -1,5 +1,4 @@
-import { constants } from "http2";
-import { arg } from "nexus";
+import { arg, intArg, nonNull } from "nexus";
 
 export const createProject = {
   args: {
@@ -10,4 +9,39 @@ export const createProject = {
     return mock_link
   },
   type: 'String'
+}
+
+
+export const updateProject = {
+  args: {
+    id : nonNull(intArg()),
+    project : nonNull(arg({type : 'ProjectUpdateInput'}))
+  },
+  resolve(_ : any, args : any, ctx : any) { // 후에 프로젝트 수정 구현
+    const mock_link = "http://mock-example.com"
+    return mock_link
+  },
+  type: 'String'
+}
+
+export const deleteProject = {
+  args: {
+    id : nonNull(intArg())
+  },
+  resolve(_ : any, args : any, ctx : any) { // 후에 프로젝트 삭제 구현
+    const mock_error = "mock error!"
+    return mock_error
+  },
+  type: 'String'
+}
+
+export const likeProject = {
+  args: {
+      id : nonNull(intArg())
+    },
+  resolve(_ : any, args : any, ctx : any) { // 후에 프로젝트 좋아요 구현
+    const mock_liked = true
+    return mock_liked
+  },
+  type: 'Boolean'
 }
