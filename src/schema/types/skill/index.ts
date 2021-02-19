d62814e4be64ea97c650ff5f0e4e4ae02ab4aa55
hanbin8269 <hanbin8269@gmail.com>
2021-02-20 00:46:28 +0900

    [DELETE] 기존 구조 변경을 위한 삭제

--- src/schema/types/skill/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './models'
-//export * from './query'
-//export * from './mutations'
-export * from './inputs'
--- src/schema/types/skill/models.ts
+++
@@ -1,8 +0,0 @@
-import { objectType } from "nexus";
-
-export const Skill = objectType({
-  name: "Skill",
-  definition(t) {
-    t.string("name");
-  },
-});
