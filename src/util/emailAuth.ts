bfef9ffe228a9b416aa0b44dd9c492ed723882d6
icefirebear <woungsub1234@gmail.com>
2021-02-19 20:44:07 +0900

    [UPDATE] refactoring sendAuthCode

--- src/util/emailAuth.ts
+++ src/util/emailAuth.ts
@@ -1,4 +1,5 @@
 import * as nodemailer from "nodemailer";
+import { PrismaClient } from "@prisma/client";
 
 // .env파일에서 정보 가져오기
 const TransporterUser: string = process.env.NODEMAILER_USER;
@@ -13,10 +14,14 @@ const transporter: nodemailer.Transporter = nodemailer.createTransport({
     },
 });
 
-export const emailAuth: Function = async (receiver: string) => {
+export const sendAuthCode: Function = async (receiver: string) => {
     //사용자의 이메일주소
-    const authCode: number = genearteAuthCode(); //인증 번호 생성
-
+    // 6자리 인증 번호 생성
+    let auth_code: any = Math.floor(Math.random() * 1000000) + 100000;
+    if (auth_code > 1000000) {
+        auth_code -= 100000;
+    }
+    auth_code = auth_code.toString();
     const mailOptions: nodemailer.SendMailOptions = {
         //메일 정보 설정
         from: `Olio development ${TransporterUser}`,
@@ -25,7 +30,7 @@ export const emailAuth: Function = async (receiver: string) => {
         text: `Olio 이메일 인증 요청 메일입니다.`,
         html: `<b>안녕하세요 ${receiver}님, Olio 포트폴리오 사이트 입니다.</b></br>
                 <b>아래 코드를 복사하여 본인 인증코드란에 입력하시기 바랍니다. </b></br>
-                <h2>인증번호: ${authCode}</h2>`,
+                <h2>인증번호: ${auth_code}</h2>`,
     };
 
     await transporter //메일 보내기
@@ -34,13 +39,4 @@ export const emailAuth: Function = async (receiver: string) => {
         .catch((err) => {
             throw new Error(err);
         });
-};
-
-// 6자리 인증 번호 생성
-const genearteAuthCode: Function = () => {
-    let authCode: number = Math.floor(Math.random() * 1000000) + 100000;
-    if (authCode > 1000000) {
-        authCode -= 100000;
-    }
-    return authCode;
 };
