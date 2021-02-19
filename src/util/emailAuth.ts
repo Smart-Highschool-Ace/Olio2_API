d0d172c22a4e99f609e78dac7a62c1b40dc102be
icefirebear <woungsub1234@gmail.com>
2021-02-19 18:44:21 +0900

    [UPDATE] add description on emailAuth.ts

--- src/util/emailAuth.ts
+++ src/util/emailAuth.ts
@@ -1,20 +1,23 @@
 import * as nodemailer from "nodemailer";
 
+// .env파일에서 정보 가져오기
 const TransporterUser = process.env.NODEMAILER_USER;
 const TransporterPass = process.env.NODEMAILER_PASS;
 
 const transporter: nodemailer.Transporter = nodemailer.createTransport({
+    //이메일 transporter 정보
     service: "gmail",
     auth: {
-        user: TransporterUser, //env 파일
+        user: TransporterUser,
         pass: TransporterPass,
     },
 });
 
 export const emailAuth: Function = async (receiver: string) => {
-    const authCode: number = genearteAuthCode();
+    const authCode: number = genearteAuthCode(); //인증 번호 생성
 
     const mailOptions: nodemailer.SendMailOptions = {
+        //메일 정보 설정
         from: `Olio development ${TransporterUser}`,
         to: `${receiver}`,
         subject: "Olio 포트폴리오 사이트 - 이메일 인증 요청 메일",
@@ -24,7 +27,7 @@ export const emailAuth: Function = async (receiver: string) => {
                 <h2>인증번호: ${authCode}</h2>`,
     };
 
-    await transporter
+    await transporter //메일 보내기
         .sendMail(mailOptions)
         .then((info) => info.messageId)
         .catch((err) => {
@@ -32,6 +35,7 @@ export const emailAuth: Function = async (receiver: string) => {
         });
 };
 
+// 6자리 인증 번호 생성
 const genearteAuthCode: Function = () => {
     let authCode = Math.floor(Math.random() * 1000000) + 100000;
     if (authCode > 1000000) {
