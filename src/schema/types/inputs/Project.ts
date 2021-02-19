69dd78bc747ffc528a9858678d6314fa6a11a1ce
hanbin8269 <hanbin8269@gmail.com>
2021-02-20 01:07:31 +0900

    [ADD] project update input 폼 생성

--- src/schema/types/inputs/Project.ts
+++ src/schema/types/inputs/Project.ts
@@ -24,6 +24,30 @@ export const ProjectCreateInput = inputObjectType({
   },
 })
 
+export const ProjectUpdateInput = inputObjectType({
+  name : 'ProjectUpdateInput',
+  definition(t){
+    t.nonNull.string('name')
+    t.nonNull.string('introduction')
+    t.string('description')
+    t.string('link')
+    t.string('start_at')
+    t.string('end_at')
+    t.list.field('skils', {
+        type : 'SkillInput'
+    })
+    t.list.field('members', {
+        type : 'ProjectMemberInput'
+    })
+    t.list.field('fields', {
+        type : 'ProjectFieldInput'
+    })
+    t.list.field('images', {
+        type : 'ProjectImageInput'
+    })
+  },
+})
+
 export const ProjectMemberInput = inputObjectType({
   name : 'ProjectMemberInput',
   definition(t){
