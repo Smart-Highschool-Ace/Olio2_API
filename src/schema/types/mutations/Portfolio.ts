c8698bf5460673551a3b3c9e087a173297e8a061
sunrabbit123 <qudwls185@naver.com>
2021-02-21 23:45:11 +0900

    [ADD] likePortfolio

--- src/schema/types/mutations/Portfolio.ts
+++ src/schema/types/mutations/Portfolio.ts
@@ -1,10 +1,17 @@
 import { arg, nonNull, intArg } from "nexus";
 
+import { context } from "../../../context";
+import {
+  getLikePortfolio,
+  createLikePortfolio,
+  deleteLikePortfolio,
+} from "service/portfolio";
+
 export const updatePortfolio = {
   args: {
     portfolio: arg({ type: "PortfolioUpdateInput" }),
   },
-  resolve: (_: any, args: any, ctx: any) => {
+  resolve: (_: any, args: any, ctx: context) => {
     // 후에 포트폴리오 수정 구현
 
     const mock_link = "http://mock-example.com";
@@ -17,9 +24,15 @@ export const likePortfolio = {
   args: {
     id: nonNull(intArg()),
   },
-  resolve: (_: any, args: any, ctx: any) => {
-    // 후에 좋아요 로직 구현
-    return true;
+  resolve: async (_: any, args: any, ctx: context) => {
+    const result = await getLikePortfolio(ctx.userId, args.id);
+    if (result) {
+      await deleteLikePortfolio(ctx.userId, args.id);
+      return false;
+    } else {
+      await createLikePortfolio(ctx.userId, args.id);
+      return true;
+    }
   },
   type: "Boolean",
 };
---
+++ src/service/portfolio.ts
@@ -0,0 +1,75 @@
+import {
+  PortfolioPrize,
+  PortfolioProject,
+  PortfolioSkill,
+  PrismaClient,
+} from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+// export const modifyPortfolio = async (
+//     id: number,
+//     email: string,
+//     skils: PortfolioSkill,
+//     projects: PortfolioProject,
+//     prize: PortfolioPrize
+//   ) => {
+//     await prisma.portfolio.update({
+//       where: {
+//         id: id,
+//       },
+//       data: {
+//         owner: {
+//           update: {
+//             email: email,
+//           },
+//         },
+//         PortfolioSkill: {
+//           set: skils,
+//         },
+//         PortfolioProject: {
+//           set: projects,
+//         },
+//         PortfolioPrize: {
+//           set: prize,
+//         },
+//       },
+//     });
+//   };
+
+export const getLikePortfolio = async (
+  user_id: number,
+  portfolio_id: number
+) => {
+  const liked = await prisma.portfolioLike.findFirst({
+    where: {
+      user_id: user_id,
+      portfolio_id: portfolio_id,
+    },
+  });
+  return liked;
+};
+
+export const createLikePortfolio = async (
+  user_id: number,
+  portfolio_id: number
+) => {
+  await prisma.portfolioLike.create({
+    data: {
+      user_id: user_id,
+      portfolio_id: portfolio_id,
+    },
+  });
+};
+
+export const deleteLikePortfolio = async (
+  user_id: number,
+  portfolio_id: number
+) => {
+  await prisma.portfolioLike.deleteMany({
+    where: {
+      user_id: user_id,
+      portfolio_id: portfolio_id,
+    },
+  });
+};
