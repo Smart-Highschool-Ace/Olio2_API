1f87406969ced78e04dfad23e3b7e543fac8fa83
hanbin8269 <hanbin8269@gmail.com>
2021-02-23 05:06:04 +0900

    [CHORE] myProjects 쿼리 리턴 타입 변경

--- src/schema/types/queries/Project.ts
+++ src/schema/types/queries/Project.ts
@@ -23,14 +23,9 @@ export const allProject = {
 };
 
 export const myProjects = {
-  type: "Project",
+  type: "MyProjectsQueryType",
   resolve: async () => {
     // TODO : 토큰의 user_id를 받아서 찾는 로직 구현
-    const mock_user_id = 1;
-    return await prisma.projectMember.findMany({
-      where: {
-        member_id: mock_user_id,
-      },
-    });
+    return;
   },
 };
