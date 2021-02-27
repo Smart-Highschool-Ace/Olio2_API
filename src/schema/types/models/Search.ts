b958167bdb2c14a39ed473d9ae5ab14b30ae0206
hanbin8269 <hanbin8269@gmail.com>
2021-02-28 03:20:11 +0900

    [CHORE] QueryType form -> Result union 으로 변경

--- src/schema/types/models/Search.ts
+++ src/schema/types/models/Search.ts
@@ -1,25 +1,33 @@
-import { objectType } from "nexus";
+import { unionType } from "nexus";
 
-export const NameSearchQueryType = objectType({
-  name: "NameSearchQueryType",
+export const NameSearchResult = unionType({
+  name: "NameSearchResult",
+  resolveType(data) {
+    const __typename = "profile_image" in data ? "User" : "Portfolio";
+    if (!__typename) {
+      throw new Error(
+        `Could not resolve the type of data passed to union type "NameSearchResult"`
+      );
+    }
+    return __typename;
+  },
   definition(t) {
-    t.list.field("portfolio", {
-      type: "Portfolio",
-    });
-    t.list.field("user", {
-      type: "User",
-    });
+    t.members("Portfolio", "User");
   },
 });
 
-export const ExploreQueryType = objectType({
-  name: "ExploreQueryType",
+export const ExploreResult = unionType({
+  name: "ExploreResult",
+  resolveType(data) {
+    const __typename = "email" in data ? "Portfolio" : "Project";
+    if (!__typename) {
+      throw new Error(
+        `Could not resolve the type of data passed to union type "NameSearchResult"`
+      );
+    }
+    return __typename;
+  },
   definition(t) {
-    t.list.field("portfolio", {
-      type: "Portfolio",
-    });
-    t.list.field("project", {
-      type: "Project",
-    });
+    t.members("Portfolio", "Project");
   },
 });
