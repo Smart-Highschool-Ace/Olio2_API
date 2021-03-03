28f649f0e3b4a255ad84917179e57de15939c593
icefirebear <woungsub1234@gmail.com>
2021-03-04 08:31:35 +0900

    [Add] createUser 구현

--- src/schema/types/mutations/User.ts
+++ src/schema/types/mutations/User.ts
@@ -1,41 +1,55 @@
 import { arg, nonNull } from "nexus";
 
+import { School } from "@prisma/client";
+import { UserService } from "service";
+
+export interface UserCreateArgs {
+    email: string;
+    password: string;
+    school: School;
+    name: string;
+    entrance_year: number;
+    profile_image?: string;
+    introduction?: string;
+}
+
 export const createUser = {
-  args: {
-    user: nonNull(arg({ type: "UserCreateInput" })),
-  },
-  resolve(_: any, args: any, ctx: any) {
-    // TODO : prisma로 create user 구현
-    const mock_user = args.user;
-    return mock_user;
-  },
-  type: "User",
+    args: {
+        user: nonNull(arg({ type: "UserCreateInput" })),
+    },
+    resolve: async (_: any, args: any, ctx: any) => {
+        const createArgs: UserCreateArgs = args.user;
+        const new_user = await UserService.createUser(createArgs);
+        return new_user;
+    },
+    type: "User",
 };
+
 export const updateUser = {
-  args: {
-    user: nonNull(arg({ type: "UserUpdateInput" })),
-  },
-  resolve(_: any, args: any, ctx: any) {
-    // TODO : prisma로 update user 구현
-    const mock_user = args.user;
-    return mock_user;
-  },
-  type: "User",
+    args: {
+        user: nonNull(arg({ type: "UserUpdateInput" })),
+    },
+    resolve(_: any, args: any, ctx: any) {
+        // TODO : prisma로 update user 구현
+        const mock_user = args.user;
+        return mock_user;
+    },
+    type: "User",
 };
 
 export const deleteUser = {
-  resolve(_: any, args: any, ctx: any) {
-    // TODO : prisma로 delete user 구현
-    const mock_user = {
-      id: 1,
-      name: "mock_user-name",
-      school: 1,
-      profile_image: "mock_user-profile_image",
-      introduction: "mock_user-introduction",
-      entrance_year: 2020,
-      grade: 1,
-    };
-    return mock_user;
-  },
-  type: "User",
+    resolve(_: any, args: any, ctx: any) {
+        // TODO : prisma로 delete user 구현
+        const mock_user = {
+            id: 1,
+            name: "mock_user-name",
+            school: 1,
+            profile_image: "mock_user-profile_image",
+            introduction: "mock_user-introduction",
+            entrance_year: 2020,
+            grade: 1,
+        };
+        return mock_user;
+    },
+    type: "User",
 };
