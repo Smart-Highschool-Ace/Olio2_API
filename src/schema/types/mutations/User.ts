a79f4d6dc0d64bba9014fdcdcb93a120d9d9b2ea
icefirebear <woungsub1234@gmail.com>
2021-03-04 14:19:44 +0900

    [Refactor] interface 수정

--- src/schema/types/mutations/User.ts
+++ src/schema/types/mutations/User.ts
@@ -4,49 +4,50 @@ import { School } from "@prisma/client";
 import { UserService } from "service";
 
 export interface UserCreateArgs {
-    email: string;
-    password: string;
-    school: School;
-    name: string;
-    entrance_year: number;
-    profile_image?: string;
-    introduction?: string;
+    user: {
+        email: string;
+        password: string;
+        school: School;
+        name: string;
+        entrance_year: number;
+        profile_image?: string;
+        introduction?: string;
+    };
 }
 
 export const createUser = {
     args: {
         user: nonNull(arg({ type: "UserCreateInput" })),
     },
-    resolve: async (_: any, args: any, ctx: any) => {
-        const createArgs: UserCreateArgs = args.user;
-        const new_user = await UserService.createUser(createArgs);
+    resolve: async (_: any, { user }: UserCreateArgs, ctx: any) => {
+        const new_user = await UserService.createUser(user);
         return new_user;
     },
     type: "User",
 };
 
 export interface UserUpdateArgs {
-    school?: School;
-    name?: string;
-    profile_image?: string;
-    introduction?: string;
+    user: {
+        school?: School;
+        name?: string;
+        profile_image?: string;
+        introduction?: string;
+    };
 }
 
 export const updateUser = {
     args: {
         user: nonNull(arg({ type: "UserUpdateInput" })),
     },
-    resolve: async (_: any, args: any, ctx: any) => {
-        const user_id = ctx.user_id;
-        const updateArgs: UserUpdateArgs = args.user;
-        const updated_user = await UserService.updateUser(user_id, updateArgs);
+    resolve: async (_: any, { user }: UserUpdateArgs, ctx: any) => {
+        const updated_user = await UserService.updateUser(ctx.user_id, user);
         return updated_user;
     },
     type: "User",
 };
 export const deleteUser = {
     resolve: async (_: any, args: any, ctx: any) => {
-        const deleted_user = await UserService.deleteUser(args.user);
+        const deleted_user = await UserService.deleteUser(ctx.user_id);
         return deleted_user;
     },
     type: "User",
