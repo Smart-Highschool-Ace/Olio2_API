bf680fae0db4795f8e7332df0ee798b715b85506
icefirebear <woungsub1234@gmail.com>
2021-03-08 23:02:52 +0900

    [Update] login mutation추가

--- src/schema/types/Mutation.ts
+++ src/schema/types/Mutation.ts
@@ -2,17 +2,18 @@ import { mutationType } from "nexus";
 import * as m from "./mutations";
 
 export const Mutation = mutationType({
-  definition(t) {
-    t.field("updatePortfolio", m.updatePortfolio);
-    t.field("likePortfolio", m.likePortfolio);
+    definition(t) {
+        t.field("updatePortfolio", m.updatePortfolio);
+        t.field("likePortfolio", m.likePortfolio);
 
-    t.field("createProject", m.createProject);
-    t.field("updateProject", m.updateProject);
-    t.field("deleteProject", m.deleteProject);
-    t.field("likeProject", m.likeProject);
+        t.field("createProject", m.createProject);
+        t.field("updateProject", m.updateProject);
+        t.field("deleteProject", m.deleteProject);
+        t.field("likeProject", m.likeProject);
 
-    t.field("createUser", m.createUser);
-    t.field("updateUser", m.updateUser);
-    t.field("deleteUser", m.deleteUser);
-  },
+        t.field("login", m.login);
+        t.field("createUser", m.createUser);
+        t.field("updateUser", m.updateUser);
+        t.field("deleteUser", m.deleteUser);
+    },
 });
