01352cce3af3b4cbee6f4c6f40227d09b0a8ec99
icefirebear <woungsub1234@gmail.com>
2021-03-08 23:00:15 +0900

    [Create] loginResult interface 모델 추가

--- src/schema/types/models/User.ts
+++ src/schema/types/models/User.ts
@@ -1,54 +1,64 @@
-import { PrismaClient } from "@prisma/client";
-import { objectType } from "nexus";
+import { interfaceType, objectType } from "nexus";
 
-import { PortfolioService, ProjectService, UserService } from "service";
-
-const prisma = new PrismaClient();
+import { PortfolioService, ProjectService } from "service";
 
 export const User = objectType({
-  name: "User",
-  definition(t) {
-    t.int("id");
-    t.string("name");
-    t.string("email");
-    t.string("school");
-    t.string("profile_image");
-    t.string("introduction");
-    t.int("entrance_year");
-    t.int("grade", {
-      resolve: (root) => {
-        return new Date().getFullYear() - root.entrance_year;
-      },
-    });
-    t.field("portfolio", {
-      type: "Portfolio",
-      resolve: async (root, _, __) => {
-        return await PortfolioService.getPortfolio(root.id);
-      },
-    });
-    t.list.field("projects", {
-      type: "Project",
-      resolve: async (root, _, __) => {
-        return await ProjectService.getOwnProjectsOfUser(root.id);
-      },
-    });
-    t.list.field("participated_projects", {
-      type: "Project",
-      resolve: async (root, _, __) => {
-        return await ProjectService.getParticipatedProjectsOfUser(root.id);
-      },
-    });
-    t.list.field("liked_projects", {
-      type: "Project",
-      resolve: async (root, _, __) => {
-        return await ProjectService.getLikedProjectsOfUser(root.id);
-      },
-    });
-    t.list.field("liked_portfolios", {
-      type: "Portfolio",
-      resolve: async (root, _, __) => {
-        return await PortfolioService.getLikedPortfoliosOfUser(root.id);
-      },
-    });
-  },
+    name: "User",
+    definition(t) {
+        t.int("id");
+        t.string("name");
+        t.string("email");
+        t.string("school");
+        t.string("profile_image");
+        t.string("introduction");
+        t.int("entrance_year");
+        t.int("grade", {
+            resolve: (root) => {
+                return new Date().getFullYear() - root.entrance_year;
+            },
+        });
+        t.field("portfolio", {
+            type: "Portfolio",
+            resolve: async (root, _, __) => {
+                return await PortfolioService.getPortfolio(root.id);
+            },
+        });
+        t.list.field("projects", {
+            type: "Project",
+            resolve: async (root, _, __) => {
+                return await ProjectService.getOwnProjectsOfUser(root.id);
+            },
+        });
+        t.list.field("participated_projects", {
+            type: "Project",
+            resolve: async (root, _, __) => {
+                return await ProjectService.getParticipatedProjectsOfUser(
+                    root.id
+                );
+            },
+        });
+        t.list.field("liked_projects", {
+            type: "Project",
+            resolve: async (root, _, __) => {
+                return await ProjectService.getLikedProjectsOfUser(root.id);
+            },
+        });
+        t.list.field("liked_portfolios", {
+            type: "Portfolio",
+            resolve: async (root, _, __) => {
+                return await PortfolioService.getLikedPortfoliosOfUser(root.id);
+            },
+        });
+    },
+});
+
+export const loginResult = interfaceType({
+    name: "loginResult",
+    resolveType(data) {
+        return "String";
+    },
+    definition(t) {
+        t.string("token");
+        t.string("error");
+    },
 });
