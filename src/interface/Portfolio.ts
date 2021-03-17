a2a598b0082a549101c4076edc6bc0e51848d11e
sunrabbit123 <qudwls185@naver.com>
2021-03-17 09:39:35 +0900

    Update interface value

--- src/interface/Portfolio.ts
+++ src/interface/Portfolio.ts
@@ -6,10 +6,9 @@ import {
 } from "@prisma/client";
 
 export interface PortfolioUpdateArgs {
-  portfolio_id: number;
-  email: string;
-  certificates: PortfolioCertificate;
-  prizes: PortfolioPrize;
-  projects: PortfolioProject;
-  skils: PortfolioSkill;
+  email?: string;
+  certificates?: PortfolioCertificate[];
+  prizes?: PortfolioPrize[];
+  projects?: PortfolioProject[];
+  skils?: PortfolioSkill[];
 }
