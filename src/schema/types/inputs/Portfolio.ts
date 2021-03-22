cd6ac26fcc8643f388474283b3e976eba803d364
sunrabbit123 <qudwls185@naver.com>
2021-03-22 09:13:37 +0900

    [MISC] Comment 반영

--- src/schema/types/inputs/Portfolio.ts
+++ src/schema/types/inputs/Portfolio.ts
@@ -22,21 +22,24 @@ export const PortfolioUpdateInput = inputObjectType({
 export const PortfolioSkillInput = inputObjectType({
   name: "PortfolioSkillInput",
   definition(t) {
-    t.nonNull.string("name"), t.nonNull.int("level");
+    t.nonNull.string("name");
+    t.nonNull.int("level");
   },
 });
 
 export const PortfolioProjectInput = inputObjectType({
   name: "PortfolioProjectInput",
   definition(t) {
-    t.nonNull.int("project_id"), t.nonNull.int("order");
+    t.nonNull.int("project_id");
+    t.nonNull.int("order");
   },
 });
 
 export const PortfolioPrizeInput = inputObjectType({
   name: "PortfolioPrizeInput",
   definition(t) {
-    t.nonNull.string("name"), t.nonNull.string("institution");
+    t.nonNull.string("name");
+    t.nonNull.string("institution");
     t.nonNull.string("prized_at");
   },
 });
@@ -44,7 +47,8 @@ export const PortfolioPrizeInput = inputObjectType({
 export const PortfolioCertificateInput = inputObjectType({
   name: "PortfolioCertificateInput",
   definition(t) {
-    t.nonNull.string("name"), t.nonNull.string("institution");
+    t.nonNull.string("name");
+    t.nonNull.string("institution");
     t.nonNull.string("certified_at");
   },
 });
