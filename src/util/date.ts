9523b414e827ee6bb8b2e2b4ff4862785d1c781e
sunrabbit123 <qudwls185@naver.com>
2021-03-23 15:58:06 +0900

    [ADD] dayjs and comment 반영

--- src/util/date.ts
+++ src/util/date.ts
@@ -1,8 +1,6 @@
+import * as dayjs from "dayjs";
+
 export const parse_yyyy_mm_dd = (dateString: any) => {
   // 입력 데이터가 스트링이지만, service/portfolio에서 사용 시 Date로 타입이 뜨기에 any로 작성함
-  const y = Number(dateString.substring(0, 4));
-  const m = Number(dateString.substring(5, 7));
-  const d = Number(dateString.substring(8, 10));
-
-  return new Date(y, m - 1, d);
+  return dayjs(dateString).toDate();
 };
