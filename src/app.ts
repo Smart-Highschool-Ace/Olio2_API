88cc25cc16376ed750f0e4d740bf03bfd8f7eb10
hmoon630 <s19017@gsm.hs.kr>
2021-06-15 15:12:15 +0900

    Remove Koa codes & Change Apollo Server to Lambda

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,7 @@
-import * as Koa from "koa";
-import * as bodyParser from "koa-bodyparser";
-import * as helmet from "koa-helmet";
-import * as cors from "@koa/cors";
-import { ApolloServer } from "apollo-server-koa";
-
+import { ApolloServer } from "apollo-server-lambda";
 import { schema } from "./schema";
 import { createContext } from "./context";
-import router from "./router";
-
-const app = new Koa();
 
 const apollo = new ApolloServer({ schema, context: createContext });
 
-app.proxy = true;
-app
-  .use(cors())
-  .use(helmet({ contentSecurityPolicy: false }))
-  .use(bodyParser())
-  .use(apollo.getMiddleware())
-  .use(router.routes())
-  .use(router.allowedMethods());
-export default app;
+exports.graphqlHandler = apollo.createHandler();
