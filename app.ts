b13054684beff92b0c5e4690701ccdfe6f9d5c0a
hmoon630 <s19017@gsm.hs.kr>
2021-06-22 14:42:51 +0900

    add: graphql playground

--- app.ts
+++ app.ts
@@ -2,6 +2,12 @@ import { ApolloServer } from "apollo-server-lambda";
 import { schema } from "./src/schema";
 import { createContext } from "./src/context";
 
-const apollo = new ApolloServer({ schema, context: createContext });
+const apollo = new ApolloServer({
+  schema,
+  context: createContext,
+  playground: {
+    endpoint: `/${process.env.STAGE || "dev"}/graphql`,
+  },
+});
 
 exports.graphqlHandler = apollo.createHandler();
