7cfc0f8619600b83d6f25523e285570046f09554
hmoon630 <s19017@gsm.hs.kr>
2021-07-05 15:00:54 +0900

    refactor: if-return

--- src/context.ts
+++ src/context.ts
@@ -1,27 +1,37 @@
-import { PrismaClient } from "@prisma/client";
-
+import { Context } from "./interface/Context";
+import { getUser } from "service/user";
 import { verifyToken } from "./util/token";
+import prisma from "./prisma";
+import { JsonWebTokenError } from "jsonwebtoken";
 
 export const createContext: Function = async ({
   event,
   context,
-}: any): Promise<{
-  userId: number;
-}> => {
-  const prisma = new PrismaClient();
+}: any): Promise<Context> => {
   const token =
     event.headers.authorization || event.headers.Authorization || null;
-  if (token) {
+
+  if (!token) {
+    return { prisma: prisma };
+  }
+
+  let user;
+  try {
     const { userId } = verifyToken(token);
+    user = await getUser(userId);
+  } catch (e) {
+    switch (e) {
+      case JsonWebTokenError:
+        return { prisma: prisma };
 
-    const user = await prisma.user.findFirst({
-      where: {
-        id: userId,
-      },
-    });
-
-    if (user) {
-      return { userId: user.id };
+      default:
+        throw e;
     }
   }
+
+  if (!user) {
+    return { prisma: prisma };
+  }
+
+  return { prisma: prisma, userId: user.id };
 };
