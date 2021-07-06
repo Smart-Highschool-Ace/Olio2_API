e06efaf191e8f4ed95091a61b0edf534fef5abfb
hmoon630 <s19017@gsm.hs.kr>
2021-07-06 11:07:01 +0900

    refactor: user service function

--- src/schema/types/queries/User.ts
+++ src/schema/types/queries/User.ts
@@ -1,5 +1,6 @@
-import { PrismaClient, User } from "@prisma/client";
+import { User } from "@prisma/client";
 import { intArg } from "nexus";
+import { UserService } from "service";
 
 import { Context } from "../../../interface";
 
@@ -14,10 +15,6 @@ export const user = {
   type: "User",
   args: { id: intArg() },
   resolve: async (_: any, args: any, ctx: Context): Promise<User> => {
-    return await ctx.prisma.user.findFirst({
-      where: {
-        id: args.id,
-      },
-    });
+    return await UserService.getUser(ctx.prisma, args.id);
   },
 };
