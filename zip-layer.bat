51dfd027a1470d0b5266308c058c290c25e1f0e3
sunrabbit123 <qudwls185@naver.com>
2022-07-09 23:16:16 +0900

    [UPDATE] layer zip codes

--- zip-layer.bat
+++ zip-layer.bat
@@ -1,5 +1,5 @@
 cd layer
 
-zip -9vr nodejs.zip ./nodejs -x ./nodejs/node_modules/@types/**
+zip -9vr nodejs.zip nodejs -x nodejs/node_modules/@types/** -x nodejs/node_modules/**/query-engine-** -x nodejs/node_modules/@prisma/engines/**
 
 cd ..
